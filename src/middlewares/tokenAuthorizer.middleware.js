1b231d20a694609951e788c44e9b5f328d995ea9
Abir Santra <santraabir8@gmail.com>
2024-01-04 19:07:58 +0530

    refactor: remove callback

--- src/middlewares/tokenAuthorizer.middleware.js
+++ src/middlewares/tokenAuthorizer.middleware.js
@@ -1,5 +1,5 @@
-import { userModel } from "../models/user.model";
-import { CustomError } from "../utils/ApiError";
+import { userModel } from "../models/user.model.js";
+import { CustomError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 
 export const tokenAuthorizer = async (req, res, next) => {
@@ -16,28 +16,21 @@ export const tokenAuthorizer = async (req, res, next) => {
       });
     }
 
-    jwt.verify(token, process.env.ACCESS_SECRET, async (error, decoded) => {
-      if (error) {
-        throw new CustomError({
-          statusCode: 403,
-          message: "Access token expired!",
-        });
-      }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_SECRET);
 
-      const user = await userModel
-        .findById(decoded.userId)
-        .select("-password -refreshToken");
+    const user = await userModel
+      .findById(decodedToken?.userId)
+      .select("-password -refreshToken");
 
-      if (!user) {
-        throw new CustomError({
-          statusCode: 401,
-          message: "Access token invalid!",
-        });
-      }
+    if (!user) {
+      throw new CustomError({
+        statusCode: 403,
+        message: "Access token invalid or expired!",
+      });
+    }
 
-      req.user = user;
-      next();
-    });
+    req.user = user;
+    next();
   } catch (error) {
     next(error);
   }
