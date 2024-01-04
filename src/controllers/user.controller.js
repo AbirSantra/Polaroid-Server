6a168338933b1ea50dff70c6be957d8ac92531c6
Abir Santra <santraabir8@gmail.com>
2024-01-04 10:46:21 +0530

    feat: add login endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,38 @@ import { CustomError } from "../utils/ApiError.js";
 import { ApiResponseHandler } from "../utils/ApiResponse.js";
 import { requiredFieldsChecker } from "../utils/requiredFieldsChecker.js";
 
+const sanitizeUser = (user) => {
+  const sanitizedUser = user.toObject();
+  delete sanitizedUser.password;
+  delete sanitizedUser.refreshToken;
+
+  return sanitizedUser;
+};
+
+const generateTokens = async (userId) => {
+  try {
+    const user = await userModel.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    const updatedUser = await user.save();
+    if (!updatedUser) {
+      throw new CustomError({
+        statusCode: 500,
+        message: "Could not save token to user",
+      });
+    }
+
+    return { accessToken, refreshToken, updatedUser };
+  } catch (error) {
+    throw new CustomError({
+      statusCode: 500,
+      message: `Error occurred while generating tokens: ${error.message}`,
+    });
+  }
+};
+
 export const registerUser = async (req, res, next) => {
   try {
     const { username, email, password, fullName } = req.body;
@@ -50,6 +82,63 @@ export const registerUser = async (req, res, next) => {
       statusCode: 200,
       message: "User created successfully",
       data: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    /* Check required field */
+    const { email, password } = req.body;
+    requiredFieldsChecker(req, ["email", "password"]);
+
+    /* Find user */
+    const user = await userModel.findOne({
+      email: email,
+    });
+    if (!user) {
+      throw new CustomError({
+        statusCode: 400,
+        message: "User not found!",
+      });
+    }
+
+    /* Match password */
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+      throw new CustomError({
+        statusCode: 400,
+        message: "Wrong password!",
+      });
+    }
+
+    /* Generate access and refresh tokens */
+    const { accessToken, refreshToken, updatedUser } = await generateTokens(
+      user._id
+    );
+
+    /* Set cookies */
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+    };
+    res.cookie("accessToken", accessToken, cookieOptions);
+    res.cookie("refreshToken", refreshToken, cookieOptions);
+
+    /* Remove unrequired fields */
+    const sanitizedUser = sanitizeUser(updatedUser);
+
+    /* Send response */
+    ApiResponseHandler({
+      res: res,
+      statusCode: 200,
+      message: "User logged in successfully",
+      data: {
+        user: sanitizedUser,
+        accessToken: accessToken,
+      },
     });
   } catch (error) {
     next(error);
